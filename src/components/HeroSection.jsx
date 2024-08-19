81dbe9b27fed0d04f7d051a04deabef611e95793
aleememon <158293046+aleememon@users.noreply.github.com>
2024-08-19 16:54:09 +0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,10 +5,10 @@ const HeroSection = () => {
   return (
     <div className="flex">
       <div className="flex-1 flex flex-col justify-center items-center md:items-start">
-        <p className="text-xl text-center md:text-left md:text-6xl ml-2 text-[#143A51] font-bold md:ml-10 md:mt-24">
+        <p className="text-lg text-center px-4 md:text-left md:text-6xl text-[#143A51] font-bold md:ml-10 md:mt-24">
           Buy Latest Labtops and Mobile Phones at Affordable Prices.
         </p>
-        <Link to={"/buynow"} className="text-white mt-5 ml-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <Link to={"/buynow"} className="text-white mt-5 ml-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Buy Now
         </Link>
       </div>

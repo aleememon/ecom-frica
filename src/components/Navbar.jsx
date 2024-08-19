d0c45a4ad6ade872cf55b423899b30898bc6a1bc
aleememon <158293046+aleememon@users.noreply.github.com>
2024-08-20 01:26:56 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const items = useSelector(state => state.items.cartItemsCount)
+  const items = useSelector((state) => state.items.cartItemsCount);
   const handleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <nav className="flex justify-between items-center m-6 text-[#143A51]">
+    <nav className="fixed top-0 left-0 w-full flex justify-between items-center p-6 bg-white shadow-md z-50 text-[#143A51]">
       {!isMenuOpen && (
         <div className="cursor-pointer" onClick={handleMenu}>
           <svg
@@ -50,7 +50,10 @@ const Navbar = () => {
             </li>
 
             <li className="hover:text-[#FF5D68] w-full duration-300 cursor-pointer">
-              <NavLink to={"/mobile-phones"} onClick={handleMenu}> Mobile Phones </NavLink>
+              <NavLink to={"/mobile-phones"} onClick={handleMenu}>
+                {" "}
+                Mobile Phones{" "}
+              </NavLink>
             </li>
             <li className="hover:text-[#FF5D68] w-full duration-300 cursor-pointer">
               <NavLink to={"/labtops"} onClick={handleMenu}>
@@ -73,17 +76,17 @@ const Navbar = () => {
       <div className="text-4xl font-bold">FRICA</div>
       <div className="relative cursor-pointer">
         <NavLink to={"/cart"}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="size-8"
-        >
-          <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
-        </svg>
-        <p className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full text-xs w-5 h-5 flex justify-center items-center ">
-          {items}
-        </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-8"
+          >
+            <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+          </svg>
+          <p className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full text-xs w-5 h-5 flex justify-center items-center ">
+            {items}
+          </p>
         </NavLink>
       </div>
     </nav>
